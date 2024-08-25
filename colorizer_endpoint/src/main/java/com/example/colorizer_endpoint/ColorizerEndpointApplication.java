60c98ccaa29c351fe2d50d656a1548d5f2b38156
markandrew8435 <markandrew8435@gmail.com>
2024-08-25 20:19:53 +0530

    initialised spring app

---
+++ colorizer_endpoint/src/main/java/com/example/colorizer_endpoint/ColorizerEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.colorizer_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorizerEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorizerEndpointApplication.class, args);
+	}
+
+}
---
+++ colorizer_endpoint/src/main/java/com/example/colorizer_endpoint/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.colorizer_endpoint;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ColorizerEndpointApplication.class);
+	}
+
+}
